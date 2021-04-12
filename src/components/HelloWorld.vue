e109a9ab0da149019f24812d0f74742367a93cdd
Alisson <damascenoalisson@gmail.com>
2021-04-12 19:39:51 -0300

    Add basic button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+      rel="stylesheet"
+    />
+
+    <div class="container instruction-text">
+      Click this button to record a song<br />
+      We will try to guess what genre it is
+    </div>
+    <div class="container">
+      <button class="button" @click="addToAmethod">
+        <i id="mic" :class="isRecordingClass"></i>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      isRecording: false,
+    };
+  },
+  computed: {
+    isRecordingClass: function () {
+      if (this.isRecording) return "fas fa-microphone microphone grow";
+      return "fas fa-microphone microphone grow is-recording";
+    },
+  },
+  methods: {
+    startRecording() {
+      console.log("Hello");
+    },
+    addToAmethod: function () {
+      console.log("triggering recording");
+      this.isRecording = !this.isRecording;
+      console.info(this.isRecording);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.instruction-text {
+  font-size: 3em;
+  color: #333333;
+  font-family: "Raleway", sans-serif;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+body {
+  background: #e2e2e2;
+  font-size: 62.5%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.microphone {
+  font-size: 4em;
+  color: #d1d1d1;
 }
-a {
-  color: #42b983;
+
+.is-recording {
+  color: #dc3545;
+}
+
+.container {
+  padding: 2em;
+}
+
+/* GENERAL BUTTON STYLING */
+button,
+button::after {
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -o-transition: all 0.5s;
+  transition: all 0.5s;
+}
+
+button {
+  background: none;
+  border: 0px solid #d1d1d1;
+  border-radius: 5px;
+  color: #fff;
+  display: block;
+  font-size: 1.6em;
+  font-weight: bold;
+  margin: 1em auto;
+  padding: 2em 6em;
+  position: relative;
+  text-transform: uppercase;
+}
+
+.grow {
+  transition: all 0.2s ease-in-out;
+}
+
+.button:hover #mic {
+  transform: scale(1.1);
+}
+
+.button {
+  border-radius: 150px;
+  background: linear-gradient(145deg, #fbfbfb, #d4d4d4);
+  box-shadow: 30px 30px 60px #c8c8c8, -30px -30px 60px #ffffff;
+}
+
+.button:hover {
+  border-radius: 150px;
+  background: #ebebeb;
+  box-shadow: 30px 30px 60px #c9c9c9, -30px -30px 60px #ffffff;
+  border: 1px solid #d1d1d1;
+}
+
+.button:active {
+  border-radius: 150px;
+  background: linear-gradient(145deg, #d4d4d4, #fbfbfb);
+  box-shadow: 30px 30px 60px #c8c8c8, -30px -30px 60px #ffffff;
 }
 </style>
